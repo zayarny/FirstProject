7e32eb0e192d66c9fb5a66125dcd4169d3de45c2
zayarny <28648638@inbox.lv>
2022-10-19 20:12:30 +0300

    Initial commit

--- src/test/java/Order.java
+++ src/test/java/Order.java
@@ -1,2 +1,87 @@
 public class Order {
+    // ATTRIBUTES
+    private double totalPrice;
+    private double weight;
+    private double volume;
+    private int itemCount;
+    private double width;
+    private double height;
+    private double depth;
+    private String address;
+    private boolean deliveryRequested;
+
+    //GETTERS/SETTERS
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isDeliveryRequested() {
+        return deliveryRequested;
+    }
+
+    public void setDeliveryRequested(boolean deliveryRequested) {
+        this.deliveryRequested = deliveryRequested;
+    }
+
 }
--- src/test/java/OrderObjectPractice.java
+++ src/test/java/OrderObjectPractice.java
@@ -1,2 +1,17 @@
+import org.junit.Test;
+
 public class OrderObjectPractice {
+    @Test
+    public void workingWithOrder() {
+        Order firstOrder = new Order();
+
+        firstOrder.setTotalPrice(12.34);
+        firstOrder.setAddress("Rigas str. 32");
+        firstOrder.setItemCount(9);
+
+        System.out.println("Hello");
+        System.out.println("First Order");
+        System.out.println(firstOrder.getTotalPrice());
+    }
+
 }
