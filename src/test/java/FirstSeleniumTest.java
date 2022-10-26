e2acecead6320f7b01ffde6db69b7144c7f99ff4
zayarny <28648638@inbox.lv>
2022-10-26 20:57:43 +0300

    Updated

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,11 +1,24 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class FirstSeleniumTest {
+    private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
+
     @Test
     public void openHomePageCheck() {
         WebDriver browser = new SafariDriver();
+        browser.manage().window().maximize();
         browser.get("http://www.discovercars.com/");
+
+        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(5, 1));
+        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
+
+        browser.findElement(ACCEPT_COOKIES_BTN).click();
     }
 }
